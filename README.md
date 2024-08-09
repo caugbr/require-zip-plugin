0d08b6cfbd6d526a2a9ca720dfcf040f36e2b93a
Cau <cau.g@dotcubed.io>
2024-08-09 09:34:49 -0300

    README; Themes support

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 ﻿# Require ZIP plugin
-Requires one or more plugins to be active. If some required plugin does not exist in the plugins folder, downloads and installs it from a URL that points to a ZIP file, such as the Github download URLs, for example.
+Requires one or more plugins or a theme to be active. If the required plugin/theme does not exist in the local folder, downloads and installs it from a URL that points to a ZIP file, such as the Github download URLs, for example.
 
-### Require a plugin
-To add some plugin to the required plugins queue, just use `$require_zip_plugin->require`. The plugin creates this global instance and you can use the `require` method, that takes four parameters:
+### Require a plugin or a theme
+To add some plugin or theme to the required items queue, just use `$require_zip_plugin->require`. The plugin creates this global instance and you can use the `require` method, that takes four parameters:
 
 - `$dependent` - Name of the requesting script
 - `$required` - Name of the reuired plugin
 - `$zip_url` - URL pointing to the ZIP file
 - `$plugin_id` - ID of the required plugin after installed, as used internally by WP (folder_name/file_name.php)
+- `$type` - Type of requested script ('plugin' | 'theme'). Default: 'plugin'
 
 ### Example
 The plugin **Inline Edit** depends on some functionalities from **WP Helper**, hosted in my Github. **Inline Edite** does not initialize if **WP Hepler** is not active and until it is activated there will be a warn in all admin pages.
@@ -23,13 +24,25 @@ The plugin **Inline Edit** depends on some functionalities from **WP Helper**, h
 	                'https://github.com/caugbr/wp-helper/archive/refs/heads/main.zip', 
 	                'wp-helper/wp-helper.php'
 	            );
+				// To require a theme
+	            $require_zip_plugin->require(
+	                'Inline Edit', 
+	                'Vue WP Theme', 
+	                'https://github.com/caugbr/wp-helper/archive/refs/heads/main.zip', 
+	                'vue-wp-theme/style.css', // ID becames the path to style.css
+					'theme' // Specify that it's a theme
+	            );
 	        }
 	        if ($wp_helper) {
+	            // ...
+	        }
+			// Test if the required theme is active
+	        if (wp_get_theme() == 'Vue WP Theme') {
 	            // ...
 	        }
 	    }
 
 ### The displayed message
-First we tell the user that script X is dependent on plugin Y. If it doesn't exist we download it and tell them so. Then we ask them to activate the plugin.
+First we tell the user that script X is dependent on plugin or theme Y. If it doesn't exist we download it and tell them so. Then we ask them to activate the plugin/theme.
 
 You can change the messages in the translation file (../langs/rzp-[your-language-code].po), observing the order of replacements (`%1$s`, `%2$s`, ...).
--- require-zip-plugin.php
+++ require-zip-plugin.php
@@ -1,10 +1,12 @@
 <?php
 /**
  * Plugin name: Require Zip Plugin
- * Description: Warns users that a plugin is required and downloads it from a ZIP URL. The warning will be displayed until the plugin is active.
+ * Description: Warns users that some plugin or theme is required and downloads it from a ZIP URL. The warning will be displayed until the plugin is active.
  * Version: 1.0
  * Author: Cau Guanabara
  * Author URI: mailto:cauguanabara@gmail.com
+ * Text Domain: rzp
+ * Domain Path: /langs/
  * License: Wordpress
  */
 
@@ -15,9 +17,25 @@ if (!defined('ABSPATH')) {
 class RequireZipPlugin {
 
     public $required = [];
+    public $strings = [];
 
     public function __construct() {
         $this->me_first();
+
+        // For translators (valid for all strings below)
+        // %1$s - requesting script
+        // %2$s - required plugin/theme
+        // %3$s - open link (if there is a link)
+        // %4$s - close link (if there is a link)
+        $this->strings['not_downloaded_not_plugins'] = __('<strong>%1$s</strong> depends on <strong>%2$s</strong> plugin, please activate it in %3$splugins page%4$s.', 'rzp');
+        $this->strings['downloaded_not_plugins'] = __('<strong>%1$s</strong> depends on <strong>%2$s</strong> plugin.<br>We just downloaded <strong>%1$s</strong> for you, please activate it in %3$splugins page%4$s.', 'rzp');
+        $this->strings['not_downloaded_plugins'] = __('<strong>%1$s</strong> depends on WP <strong>%2$s</strong> plugin, please activate it.', 'rzp');
+        $this->strings['downloaded_plugins'] = __('<strong>%1$s</strong> depends on <strong>%2$s</strong> plugin.<br>We just downloaded <strong>%1$s</strong> for you, but since you are in the plugins page, you must %3$sreload the page%4$s to see it.', 'rzp');
+        $this->strings['not_downloaded_not_themes'] = __('<strong>%1$s</strong> depends on WP <strong>%2$s</strong> theme, please activate it in %3$sthemes page%4$s.', 'rzp');
+        $this->strings['downloaded_not_themes'] = __('<strong>%1$s</strong> depends on <strong>%2$s</strong> theme.<br>We just downloaded <strong>%1$s</strong> for you, please activate it in %3$sthemes page%4$s.', 'rzp');
+        $this->strings['not_downloaded_themes'] = __('<strong>%1$s</strong> depends on <strong>%2$s</strong> theme, please activate it.', 'rzp');
+        $this->strings['downloaded_themes'] = __('<strong>%1$s</strong> depends on WP <strong>%2$s</strong> theme.<br>We just downloaded <strong>%1$s</strong> for you, but since you are in the themes page, you must %3$sreload the page%4$s to see it.', 'rzp');
+
         add_action('init', [$this, 'load_translations']);
         add_action('admin_notices', [$this, 'add_notices']);
     }
@@ -26,21 +44,24 @@ class RequireZipPlugin {
      * Add required plugin to queue
      *
      * @param [type] $dependent     Name of the script that are requiring
-     * @param [type] $required      Name of the requested plugin
+     * @param [type] $required      Name of the requested plugin/theme
      * @param [type] $zip_url       URL to the ZIP file
-     * @param [type] $plugin_id     Plugin ID (plugin_directory_name/plugin_file_name.php)
+     * @param [type] $plugin_id     Plugin ID (plugin_directory_name/plugin_file_name.php or theme_directory_name/style.css)
      * @return void
      */
-    public function require($dependent, $required, $zip_url, $plugin_id) {
-        $this->required[] = compact('dependent', 'required', 'zip_url', 'plugin_id');
+    public function require($dependent, $required, $zip_url, $plugin_id, $type = 'plugin') {
+        $this->required[] = compact('dependent', 'required', 'zip_url', 'plugin_id', 'type');
     }
     
     public function load_translations() {
         load_plugin_textdomain('rzp', false, dirname(plugin_basename(__FILE__)) . '/langs');
     }
 
-    private function check($plugin_id) {
-        return file_exists(ABSPATH . "/wp-content/plugins/{$plugin_id}");
+    private function check($item) {
+        if ($item['type'] == 'theme') {
+            return file_exists(ABSPATH . "/wp-content/themes/{$item['plugin_id']}");
+        }
+        return file_exists(ABSPATH . "/wp-content/plugins/{$item['plugin_id']}");
     }
 
     public function me_first() {
@@ -55,53 +76,75 @@ class RequireZipPlugin {
 
     public function add_notices() {
         foreach ($this->required as $info) {
-            $downloaded = false;
-            if (!$this->check($info['plugin_id'])) {
-                $downloaded = $this->download($info['zip_url'], $info['plugin_id']);
-            }
-            global $pagenow;
-            if (!is_plugin_active($info['plugin_id'])) {
-                print '<div class="notice notice-error"><p>';
-                // For translators
-                // %1$s - requesting script
-                // %2$s - required plugin
-                printf(__('<strong>%1$s</strong> depends on <strong>%2$s</strong> plugin', 'rzp'), $info['dependent'], $info['required']);
-                if ($pagenow == 'plugins.php') {
-                    if ($downloaded) {
-                        // %1$s - required plugin
-                        // %2$s - open link
-                        // %3$s - close link
-                        printf(__('.<br>We have downloaded <strong>%1$s</strong> for you, but since you are in the plugins page, you must %2$sreload the page%3$s to see it.', 'rzp'), $info['required'], '<a href="javascript:location.reload()">', '</a>');
-                    } else {
-                        // %1$s - required plugin
-                        printf(__(', please activate it.', 'rzp'), $info['required']);
-                    }
-                } else {
-                    if ($downloaded) {
-                        // %1$s - required plugin
-                        // %2$s - open link
-                        // %3$s - close link
-                        printf(__('.<br>We have downloaded <strong>%1$s</strong> for you, just activate it in %2$splugins page%3$s.'), $info['required'], '<a href="plugins.php">', '</a>');
-                    } else {
-                        // %1$s - required plugin
-                        // %2$s - open link
-                        // %3$s - close link
-                        printf(__(', please activate it in %2$splugins page%3$s.', 'rzp'), $info['required'], '<a href="plugins.php">', '</a>');
-                    }
-                }
-                print '</p></div>';
-            }
+            $this->{"add_{$info['type']}_notice"}($info);
         }
     }
 
-    private function download($zip_url, $plugin_id) {
-        $dir_name = explode("/", $plugin_id)[0];
-        $parts = explode("/", $zip_url);
+    public function add_theme_notice($info) {
+        $downloaded = false;
+        if (!$this->check($info)) {
+            $downloaded = $this->download($info);
+        }
+        global $pagenow;
+        print "THEME: " . wp_get_theme() . "\n";
+        if (wp_get_theme() != $info['required']) {
+            $params = [$info['dependent'], $info['required']];
+            print '<div class="notice notice-error"><p>';
+            if ($pagenow == 'themes.php') {
+                if ($downloaded) {
+                    $params = array_merge($params, ['<a href="javascript:location.reload()">', '</a>']);
+                    printf($this->strings['downloaded_themes'], ...$params);
+                } else {
+                    printf($this->strings['not_downloaded_themes'], ...$params);
+                }
+            } else {
+                $params = array_merge($params, ['<a href="themes.php">', '</a>']);
+                if ($downloaded) {
+                    printf($this->strings['downloaded_not_themes'], ...$params);
+                } else {
+                    printf($this->strings['not_downloaded_not_themes'], ...$params);
+                }
+            }
+            print '</p></div>';
+        }
+    }
+
+    public function add_plugin_notice($info) {
+        $downloaded = false;
+        if (!$this->check($info)) {
+            $downloaded = $this->download($info);
+        }
+        global $pagenow;
+        if (!is_plugin_active($info['plugin_id'])) {
+            $params = [$info['dependent'], $info['required']];
+            print '<div class="notice notice-error"><p>';
+            if ($pagenow == 'plugins.php') {
+                if ($downloaded) {
+                    $params = array_merge($params, ['<a href="javascript:location.reload()">', '</a>']);
+                    printf($this->strings['downloaded_plugins'], ...$params);
+                } else {
+                    printf($this->strings['not_downloaded_plugins'], ...$params);
+                }
+            } else {
+                $params = array_merge($params, ['<a href="plugins.php">', '</a>']);
+                if ($downloaded) {
+                    printf($this->strings['downloaded_not_plugins'], ...$params);
+                } else {
+                    printf($this->strings['not_downloaded_not_plugins'], ...$params);
+                }
+            }
+            print '</p></div>';
+        }
+    }
+
+    private function download($item) {
+        $dir_name = explode("/", $item['plugin_id'])[0];
+        $parts = explode("/", $item['zip_url']);
         $name = $parts[count($parts) - 1];
         $local_file = ABSPATH . "wp-content/uploads/{$name}";
         $wp_filesystem = filesystem();
     
-        $data = wp_remote_get($zip_url);
+        $data = wp_remote_get($item['zip_url']);
         $zip = $data['body'];
         $wp_filesystem->put_contents($local_file, $zip);
     
@@ -110,7 +153,7 @@ class RequireZipPlugin {
             $dirs = array_values(array_diff(scandir($unzip_dir), ['.', '..']));
             $dir = $dirs[0] ?? '';
             if (!empty($dir) && is_dir($unzip_dir . $dir)) {
-                $wp_filesystem->move($unzip_dir . $dir, ABSPATH . "/wp-content/plugins/{$dir_name}");
+                $wp_filesystem->move($unzip_dir . $dir, ABSPATH . "/wp-content/{$item['type']}s/{$dir_name}");
                 unlink($local_file);
                 rmdir($unzip_dir);
                 return true;
